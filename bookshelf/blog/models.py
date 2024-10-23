cebadad548cc657415f93e2a85a11bec1df12616
Krushovice <kickstar69@yandex.ru>
2024-10-23 16:15:01 +1000

    create Comment model

--- bookshelf/blog/models.py
+++ bookshelf/blog/models.py
@@ -64,32 +64,32 @@ class Post(models.Model):
         return self.title
 
 
-# class Comment(models.Model):
-#     class Meta:
-#         ordering = ["-created"]
-#         indexes = [
-#             models.Index(fields=["-created"]),
-#         ]
-#
-#     post = models.ForeignKey(
-#         Post,
-#         on_delete=models.CASCADE,
-#         related_name="comments",
-#     )
-#     name = models.CharField(max_length=80)
-#     email = models.EmailField()
-#     body = models.TextField()
-#     created = models.DateTimeField(auto_now_add=True)
-#     updated = models.DateTimeField(auto_now=True)
-#     active = models.BooleanField(default=True)
-#
-#     def get_absolute_url(self):
-#         return reverse(
-#             "blog:post_comment",
-#             args=[
-#                 self.pk,
-#             ],
-#         )
-#
-#     def __str__(self):
-#         return f"Comment by {self.name} on {self.post}"
+class Comment(models.Model):
+    class Meta:
+        ordering = ["-created"]
+        indexes = [
+            models.Index(fields=["-created"]),
+        ]
+
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="comments",
+    )
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    def get_absolute_url(self):
+        return reverse(
+            "blog:post_comment",
+            args=[
+                self.pk,
+            ],
+        )
+
+    def __str__(self):
+        return f"Comment by {self.name} on {self.post}"
