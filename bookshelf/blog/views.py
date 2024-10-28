77706ccfd50e3d4ccb40e635f93f3972974e30da
Krushovice <kickstar69@yandex.ru>
2024-10-28 17:11:00 +1000

    add weighting queries to post search

--- bookshelf/blog/views.py
+++ bookshelf/blog/views.py
@@ -1,6 +1,12 @@
 from audioop import reverse
+from re import search
 
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import (
+    SearchVector,
+    SearchQuery,
+    SearchRank,
+)
+
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
 
@@ -185,9 +191,22 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data["query"]
-            results = Post.published.annotate(
-                search=SearchVector("title", "body"),
-            ).filter(search=query)
+            search_vector = SearchVector(
+                "title",
+                weight="A",
+            ) + SearchVector(
+                "body",
+                weight="B",
+            )
+            search_query = SearchQuery(query)
+            results = (
+                Post.published.annotate(
+                    search=search_vector,
+                    rank=SearchRank(search_vector, search_query),
+                )
+                .filter(rank__gte=0.3)
+                .order_by("-rank")
+            )
 
     return render(
         request,
