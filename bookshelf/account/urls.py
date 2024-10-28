b25c2891b18fc733e7e49f61f84409514df4ce63
Krushovice <kickstar69@yandex.ru>
2024-10-28 21:43:10 +1000

    create urld for password change

--- bookshelf/account/urls.py
+++ bookshelf/account/urls.py
@@ -12,4 +12,14 @@ urlpatterns = [
     path("", views.get_profile, name="profile"),
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path(
+        "password-change/",
+        auth_views.PasswordChangeView.as_view(),
+        name="password_change",
+    ),
+    path(
+        "password-change/done/",
+        auth_views.PasswordChangeDoneView.as_view(),
+        name="password_change_done",
+    ),
 ]
