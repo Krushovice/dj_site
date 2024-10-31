3cbea96c02284f4add98831134a935c5e035709b
Krushovice <kickstar69@yandex.ru>
2024-10-31 16:21:23 +1000

    create register user view

--- bookshelf/account/views.py
+++ bookshelf/account/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from .forms import UserRegistrationForm
 
 # Create your views here.
 
@@ -11,4 +12,25 @@ def get_profile(request):
         request,
         "account/profile.html",
         {"section": "profile"},
+    )
+
+
+def register(request):
+    if request.method == "POST":
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data["password"])
+            new_user.save()
+            return render(
+                request,
+                "account/register_done.html",
+                {"new_user": new_user},
+            )
+    else:
+        user_form = UserRegistrationForm()
+    return render(
+        request,
+        "account/register.html",
+        {"user_form": user_form},
     )
