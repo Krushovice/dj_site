021f4fd8a74525c43e66c9749dca63a7a1806c82
Krushovice <kickstar69@yandex.ru>
2024-10-31 17:20:53 +1000

    register edit profile view

--- bookshelf/account/urls.py
+++ bookshelf/account/urls.py
@@ -56,4 +56,5 @@ urlpatterns = [
     path("", include("django.contrib.auth.urls")),
     path("", views.get_profile, name="profile"),
     path("register/", views.register, name="register"),
+    path("edit-profile/", views.edit_profile, name="edit_profile"),
 ]
