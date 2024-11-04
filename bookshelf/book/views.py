e8de14a27771e913afb3ad1814148651148ccb48
Krushovice <kickstar69@yandex.ru>
2024-11-04 12:04:42 +1000

    create index view

--- bookshelf/book/views.py
+++ bookshelf/book/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Book
 
 # Create your views here.
+
+
+def index(request):
+    books = Book.objects.all()
+
+    return render(
+        request,
+        "book/index.html",
+        context={"books": books},
+    )
