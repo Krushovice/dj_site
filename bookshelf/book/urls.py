c5e4e67c6057cf20748a60943129f201818686b5
Krushovice <kickstar69@yandex.ru>
2024-11-04 12:27:06 +1000

    create ListView class instead of index func

--- bookshelf/book/urls.py
+++ bookshelf/book/urls.py
@@ -6,6 +6,6 @@ from . import views
 app_name = "books"
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.IndexView.as_view(), name="index"),
     # path("top-list/", views.top_list, name="top_list"),
 ]
